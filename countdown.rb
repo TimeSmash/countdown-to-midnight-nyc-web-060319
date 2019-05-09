df44d8b38b80bb766147f9e00a8fb1a726753ae3
mcumm64 <mcumm64@gmail.com>
2019-05-09 21:18:30 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,7 +4,16 @@ def countdown(int)
   while int > 0
     puts "#{int} SECOND(S)!"
     int -= 1
-    sleep(1_secs)
+  end
+  "HAPPY NEW YEAR!" 
+end
+
+
+def countdown(int)
+  while int > 0
+    puts "#{int} SECOND(S)!"
+    int -= 1
+    sleep 1
   end
   "HAPPY NEW YEAR!" 
 end
